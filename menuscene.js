c2f5947153ec6311a4a7c2575da895a406e4dfad
Björn Öhnell <bjorn.ohnell@gmail.com>
2024-04-10 09:09:07 +0200

    buggfix

--- menuscene.js
+++ menuscene.js
@@ -2,21 +2,23 @@ var MenuScene = new Phaser.Class({
     Extends: Phaser.Scene,
     initialize: function() {
         Phaser.Scene.call(this, { "key": "MenuScene" });
-        console.log("Kom hit")
+   
     },
     init: function() {
     },
     preload: function() {
+        console.log("Kom hit")
         this.load.image('menu', 'assets/menubackground.png');
         this.input.on('pointerdown', function(pointer) {
-            this.scene.start('CreditsScene');
+            this.scene.start('TextScene');
 
-        }, this);   
+        }, this); 
 
 
     },
     create: function() {
-
+        var background = this.add.image(0,0,'menu')
+        background.setOrigin(0,0)
     },
     update: function() {}
 });
